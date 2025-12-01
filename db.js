cbfdc491a5dfe026ded651f7ca149389dda3d60a
Rahul Thakur <98264143+trahul1125@users.noreply.github.com>
2025-12-01 18:27:43 +0530

    Update db.js

--- db.js
+++ db.js
@@ -4,6 +4,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const connectionString = process.env.DATABASE_URL
-const sql = postgres(connectionString)
+const sql = postgres(connectionString, {
+  ssl: 'require',
+  connection: {
+    options: '--search_path=public'
+  }
+})
 
 export default sql
